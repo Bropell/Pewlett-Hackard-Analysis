2a341a4238781c88be450dbc3e70441512ec35af
Brandon Ropell <brandon.ropell@gmail.com>
2022-07-15 14:50:31 -0400

    Added Queries folder with in-class and challenge queries

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,41 @@
+SELECT emp_no, first_name, last_name
+FROM employees;
+
+SELECT title, from_date, to_date
+FROM titles;
+
+
+-- Create new table by joining employees table and titles table on primary keys
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM employees as e
+JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM retirement_titles;
+
+
+-- Remove duplicate entries for employees 
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM unique_titles;
+
+
+-- Retrieve the number of employees by their most recent job title who are about to retire
+SELECT COUNT(title) as Count, title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY Count DESC;
+
+SELECT* FROM retiring_titles;
